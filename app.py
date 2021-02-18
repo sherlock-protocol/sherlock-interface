42c7182ce0ec49df03bea02f8ba561f4b7469d0d
Evert Kors <evert@hatching.io>
2021-02-18 21:21:57 +0100

    Add routings

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 import settings
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder="templates")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template(
+        'dashboard.html',
+    )
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def allocations():
+    return render_template(
+        'allocations.html',
+    )
+
+
+@app.route('/')
+def landing():
+    return render_template(
+        'landing.html',
+    )
 
 if __name__ == '__main__':
     app.run(host=settings.SERVER_HOST, port=settings.SERVER_PORT)
