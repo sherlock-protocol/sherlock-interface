5f479a328ef2d5eaf13a6f0b2a178b5b130549fc
Evert Kors <evert@hatching.io>
2021-02-18 22:30:16 +0100

    Add some data

--- app.py
+++ app.py
@@ -7,12 +7,20 @@ app = Flask(__name__, template_folder="templates")
 def dashboard():
     return render_template(
         'dashboard.html',
+        title='Dashboard',
+        desc='dashboard',
+        tags=["dashboard"],
+        currentPage="dashboard",
     )
 
 @app.route('/allocations')
 def allocations():
     return render_template(
         'allocations.html',
+        title='Allocations',
+        desc='allocations',
+        tags=["allocations"],
+        currentPage="allocations",
     )
 
 
@@ -20,6 +28,10 @@ def allocations():
 def landing():
     return render_template(
         'landing.html',
+        title='Landing',
+        desc='landing',
+        tags=["landing"],
+        currentPage="landing",
     )
 
 if __name__ == '__main__':
