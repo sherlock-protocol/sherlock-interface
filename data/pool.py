eb74b4ecb48b78c72e8e9164ba0bf56225f38af7
Evert Kors <evert@hatching.io>
2021-02-20 13:59:43 +0100

    Add lockup days in pool data

--- data/pool.py
+++ data/pool.py
@@ -1,8 +1,8 @@
 import settings
 
 def get_staking_pool_data():
-    return {'staked_funds': "15,069,244.62", 'apy': "11.39"}
-
+    return {'staked_funds': "15,069,244.62", 'apy': "11.39", "lockup": "3"}
+    # todo lockup
     stakedFunds = settings.POOL_CONTRACT_HTTP.functions.getTotalStakedFunds().call()
     # todo calculate apy
     return {
