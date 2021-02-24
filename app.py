e820ac38ee21b9fee61653bb24543773dbd70472
Vincent Schut <flessendop@hatching.io>
2021-02-24 11:21:10 +0100

    Updated FAQ

--- app.py
+++ app.py
@@ -52,38 +52,7 @@ def faq():
         desc='faq',
         tags=["faq"],
         currentPage="faq",
-        data={
-            "faq": [
-                {
-                    "q": "How does it work?",
-                    "a": "Audits R Dead is an insurance platform designed to provide protocols with affordable, high-quality coverage against smart contract hacks. Anyone can stake in our insurance pool and we aim to offer the highest risk-adjusted return in DeFi. This is made possible by our expert smart contract security team, who reviews and prices insurance for every protocol we cover and has “skin-in-the-game” alongside our stakers."
-                },
-                {
-                    "q": "How is it different from other insurance protocols?",
-                    "a": "We think existing insurance is unpopular for two major reasons: 1) Poor user experience and 2) high prices. 1) Nobody likes buying insurance which is why we don’t make you do it. We cover protocols wholesale so users don’t need to worry about it. 2) Existing protocols outsource pricing by either making users decide which protocols are safe or by relying on old audits. We have a team of expert smart contract security analysts who price our insurance and have “skin in the game” alongside our stakers."
-                },
-                {
-                    "q": "Why are audits dead?",
-                    "a": "A major reason many protocols pay for expensive audits is to prevent user funds from being stolen. Over the last few months, it has become increasingly clear that audits are not a perfect solution for preventing hacks. We don’t think there is a perfect solution to preventing hacks. Instead, we think the risk of loss should be shifted from unsuspecting users to investors who are looking for a favorable risk-adjusted return. That’s why we built this protocol."
-                },
-                {
-                    "q": "How does staking work?",
-                    "a": "Stakers put their funds in our pool and receive a yield based on protocol insurance fees and current lending protocol rates. These funds cover the risk of smart contract hacks among our partner protocols and can be slashed in the event of a hack. There is currently a 3-day “unlocking” period to withdraw funds from the pool."
-                },
-                {
-                    "q": "How can I get insurance as a protocol?",
-                    "a": "We are currently in a closed beta with a select number of protocols. Please email us at auditsrdead@gmail.com to join the waitlist."
-                },
-                {
-                    "q": "How do I start using it on Kovan?",
-                    "a": "Get Kovan ETH here. </br>Get Kovan Aave Dai here."
-                },
-                {
-                    "q": "Where can I see the contracts this protocol interacts with?",
-                    "a": ""
-                }
-            ]
-        },
+        data={},
         wallet=request.cookies.get('wallet'),
         env=env()
     )
