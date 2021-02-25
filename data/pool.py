5daf2dbf0f19e89eca76e5ea8b20476dd7b0c919
Evert Kors <evert@hatching.io>
2021-02-25 20:12:16 +0100

    Add pool data

--- data/pool.py
+++ data/pool.py
@@ -1,17 +1,23 @@
 import settings
+from web3 import Web3
 
 def get_staking_pool_data():
-    return {'staked_funds': "15,069,244.62", 'apy': "11.39", "lockup": "3"}
+    # return {'staked_funds': "15,069,244.62", 'apy': "11.39", "lockup": "3"}
     # todo lockup
+
     stakedFunds = settings.POOL_CONTRACT_HTTP.functions.getTotalStakedFunds().call()
     # todo calculate apy
+    # todo lockup
     return {
-        "staked_funds": stakedFunds,
-        "apy": 12544430000000000000
+        "staked_funds": '{:,.2f}'.format(Web3.fromWei(stakedFunds, 'ether')),
+        "staked_funds_big": stakedFunds,
+        "yield": "500000000000000000",
+        "apy": "11.39",
+        "lockup": 3
     }
 
 def get_covered_protocols():
-    return [{'id': '2698812145d22d42d61c043b9933d0771afdf0fad79a42fc985105d0f27141b0', 'name': 'Maker', 'covered': '11000000000000000000000000', 'premium': 95129375946000000, 'balance': 0, 'last_paid': 23343713, 'profile': [11000000000000000000000000, 8648125086]}, {'id': 'd88eb2629f4cc00a052d4e86cd29a0be2b39e7b0c2fe1c459f7e4c1aa3d4df3b', 'name': 'Yearn', 'covered': '9000000000000000000000000', 'premium': 77833125774000000, 'balance': 0, 'last_paid': 23343719, 'profile': [9000000000000000000000000, 8648125086]}, {'id': 'c94af803b7bf4f45b7e9822e53806f6d873074abc6cedbfccfa727bf00c623e6', 'name': 'PieDao', 'covered': '5000000000000000000000000', 'premium': 43240625430000000, 'balance': 0, 'last_paid': 23343725, 'profile': [5000000000000000000000000, 8648125086]}]
+    #return [{'id': '2698812145d22d42d61c043b9933d0771afdf0fad79a42fc985105d0f27141b0', 'name': 'Maker', 'covered': '11000000000000000000000000', 'premium': 95129375946000000, 'balance': 0, 'last_paid': 23343713, 'profile': [11000000000000000000000000, 8648125086]}, {'id': 'd88eb2629f4cc00a052d4e86cd29a0be2b39e7b0c2fe1c459f7e4c1aa3d4df3b', 'name': 'Yearn', 'covered': '9000000000000000000000000', 'premium': 77833125774000000, 'balance': 0, 'last_paid': 23343719, 'profile': [9000000000000000000000000, 8648125086]}, {'id': 'c94af803b7bf4f45b7e9822e53806f6d873074abc6cedbfccfa727bf00c623e6', 'name': 'PieDao', 'covered': '5000000000000000000000000', 'premium': 43240625430000000, 'balance': 0, 'last_paid': 23343725, 'profile': [5000000000000000000000000, 8648125086]}]
     data = []
 
     amountOfProtocolsCovered = settings.POOL_CONTRACT_HTTP.functions.amountOfProtocolsCovered().call()
@@ -25,16 +31,16 @@ def get_covered_protocols():
         data.append({
             "id": str(id.hex()),
             "name": settings.PROTOCOL_NAMES.get(str(id.hex())),
-            "covered":coveredFunds,
-            "premium":premiumPerBlock,
-            "balance":balance,
-            "last_paid":lastPaid,
-            "profile":profile
+            "covered":str(coveredFunds),
+            "premium":str(premiumPerBlock),
+            "balance":str(balance),
+            "last_paid":str(lastPaid),
+            "profile":str(profile)
         })
     return data
 
 def get_pool_strategies():
-    return [{'name': 'ATokenV2StrategyToAave', 'token_address': '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD', 'strategy_address': '0x5852A4a9687dAFFCd5464a2790d3F4d5E5001A69', 'oracle_address': '0x0000000000000000000000000000000000000000', 'balance': "14121785424705346597586066"}, {'name': 'AaveStrategyToUniswap', 'token_address': '0x85821C543d5773cA19b91F5b37e39FeC308C6FA7', 'strategy_address': '0xBb8974C5F93ED2935E4E0d9abC95551310c48F62', 'oracle_address': '0xb3Ef934755f162e2Aa1c7Aae4CD6167aE2694d25', 'balance': "182203692109052093154252"}]
+    #   return [{'name': 'ATokenV2StrategyToAave', 'token_address': '0xFf795577d9AC8bD7D90Ee22b6C1703490b6512FD', 'strategy_address': '0x5852A4a9687dAFFCd5464a2790d3F4d5E5001A69', 'oracle_address': '0x0000000000000000000000000000000000000000', 'balance': "14121785424705346597586066"}, {'name': 'AaveStrategyToUniswap', 'token_address': '0x85821C543d5773cA19b91F5b37e39FeC308C6FA7', 'strategy_address': '0xBb8974C5F93ED2935E4E0d9abC95551310c48F62', 'oracle_address': '0xb3Ef934755f162e2Aa1c7Aae4CD6167aE2694d25', 'balance': "182203692109052093154252"}]
     data = []
     amountOfStrategies = settings.SM_CONTRACT_HTTP.functions.amountOfStrategies().call()
     for i in range(amountOfStrategies):
@@ -51,7 +57,7 @@ def get_pool_strategies():
             "token_address": token,
             "strategy_address": strategy,
             "oracle_address":oracle,
-            "balance":balance,
+            "balance":str(balance),
         })
     return data
 
